928ebad7752164cb763c09871179652fe0a53243
JAEEUN SHIN <122805045+JaeeunSkywalker@users.noreply.github.com>
2023-10-05 00:59:41 +0900

    Remove about_user.dart from tracking

--- lib/consts/about_user.dart
+++
@@ -1,4 +0,0 @@
-const String clientId = '23R8HC';
-const String scope =
-    'activity cardio_fitness electrocardiogram heartrate location nutrition oxygen_saturation profile respiratory_rate settings sleep social temperature weight';
-const String redirectUrl = 'https://still-incomplete.com';
